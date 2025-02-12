bdedf10c254609133645b12acaa39c6e14691e8b
VindeeR <ericklopes.47@gmail.com>
2025-02-12 17:40:08 +0100

    commiting a simpler version of the test to ensure test work as local

--- test/unit/test_autosubmit.py
+++ test/unit/test_autosubmit.py
@@ -7,31 +7,14 @@ from test.unit.conftest import autosubmit_config
 from typing import Callable, Dict
 
 
-@pytest.mark.parametrize("fake_dir, real_dir", [
-    pytest.param("a000", "a000", marks=pytest.mark.xfail(reason="Meant to fail since it can't create a folder if one already exists")), # test meant to FAIL
-    ("","a000"), # test meant to PASS
-    ("",""), ]) # test meant to PASS with a generated expid
-def test_expid(autosubmit_config: Callable[[str,Dict], BasicConfig], fake_dir, real_dir) -> None:
+def test_expid(autosubmit_config: Callable[[str,Dict], BasicConfig]) -> None:
     """
     Function to test if the autosubmit().expid generates the paths and expid properly
-
-    ::fake_dir -> if fake dir exists test will fail since it won't be able to generate folder
-    ::real_dir -> folder it'll try to create and experiment id
     """
 
-    if fake_dir != "":
-        path = Path(BasicConfig.LOCAL_ROOT_DIR) / fake_dir
-        path.mkdir()
-
-    if real_dir != "":
-        autosubmit_config(real_dir, {})
-    expid = Autosubmit().expid("Test", real_dir)
-
-    if real_dir == "":
-        autosubmit_config(expid, {})
-
+    autosubmit_config('a000', {})
+    expid = Autosubmit().expid("Test")
     experiment = Autosubmit().describe(expid)
-
     path = Path(BasicConfig.LOCAL_ROOT_DIR) / expid
 
     assert path.exists()
