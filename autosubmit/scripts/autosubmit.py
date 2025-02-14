1ddb25350d6720b03cfcf409721062310f9aa112
Irene Simo Munoz <irene.simomunoz@bsc.es>
2025-02-14 09:31:38 +0100

    Simplify version test

--- autosubmit/scripts/autosubmit.py
+++ autosubmit/scripts/autosubmit.py
@@ -29,11 +29,6 @@ sys.path.append(script_dir)
 sys.path.append(os.path.normpath(os.path.join(script_dir, os.pardir)))
 # noinspection PyUnresolvedReferences
 from log.log import Log, AutosubmitCritical, AutosubmitError  # noqa: E402
-import sys
-import os
-
-# Add the directory containing the `autosubmit` module to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 from autosubmit import delete_lock_file, exit_from_error  # noqa: E402
 from autosubmit.autosubmit import Autosubmit  # noqa: E402
