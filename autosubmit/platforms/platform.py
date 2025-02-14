57a6378a5b61f87ba3f1c91fbd5150e85966bac2
dbeltrankyl <daniel.beltran@bsc.es>
2025-02-14 15:13:01 +0100

    update default value

--- autosubmit/platforms/platform.py
+++ autosubmit/platforms/platform.py
@@ -61,7 +61,7 @@ class UniqueQueue(Queue):
     A queue that avoids retrieves the same job and retrial during the same run.
     """
 
-    def __init__(self, maxsize: int = -1, block: bool = True, timeout: float = None, max_items: int = 10):
+    def __init__(self, maxsize: int = -1, block: bool = True, timeout: float = None, max_items: int = 200):
         """
         Initializes the UniqueQueue.
 
@@ -69,7 +69,7 @@ class UniqueQueue(Queue):
             maxsize (int): Maximum size of the queue. Defaults to -1 (infinite size).
             block (bool): Whether to block when the queue is full. Defaults to True.
             timeout (float): Timeout for blocking operations. Defaults to None.
-            max_items (int): Maximum number of unique items to track. Defaults to 1000.
+            max_items (int): Maximum number of unique items to track. Defaults to 200.
 
         """
         self.block = block
@@ -1008,7 +1008,6 @@ class Platform(object):
             bool: True if there is work to process, False otherwise.
         """
         process_log = False
-        print(timeout)
         while timeout > 0:
             if not self.recovery_queue.empty() or self.cleanup_event.is_set() or self.work_event.is_set():
                 process_log = True
