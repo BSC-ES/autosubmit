f694fa619c2fa8c4ee9fb095c2886f602e05ea74
VindeeR <ericklopes.47@gmail.com>
2025-02-24 14:24:16 +0100

    adjusting order of the assignments and adding rerturn to the function

--- test/unit/test_dependencies.py
+++ test/unit/test_dependencies.py
@@ -283,6 +283,10 @@ class TestJobList(unittest.TestCase):
         }
         self.assertEqual(result, expected_output)
 
+        self.relationships_dates["DATES_FROM"]["20020201"]["MEMBERS_FROM"] = {}
+        self.relationships_dates["DATES_FROM"]["20020201"]["CHUNKS_FROM"] = {}
+        self.relationships_dates["DATES_FROM"]["20020201"]["SPLITS_FROM"] = {}
+
         result = self.JobList._check_dates(self.relationships_dates, self.mock_job)
         expected_output = {
             "DATES_TO": "none",
@@ -296,10 +300,6 @@ class TestJobList(unittest.TestCase):
         self.mock_job.date = datetime.strptime("20020301", "%Y%m%d")
         result = self.JobList._check_dates(self.relationships_dates, self.mock_job)
         self.assertEqual(result, {})
-
-        self.relationships_dates["DATES_FROM"]["20020201"]["MEMBERS_FROM"] = {}
-        self.relationships_dates["DATES_FROM"]["20020201"]["CHUNKS_FROM"] = {}
-        self.relationships_dates["DATES_FROM"]["20020201"]["SPLITS_FROM"] = {}
 
 
     def test_check_members(self):
