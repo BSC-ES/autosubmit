f879851a314e4ad74d0a7f9861454f1f15255225
Erick Lopes <30758033+VindeeR@users.noreply.github.com>
2025-03-03 17:33:10 +0100

    Update test_diagram.py
    library deleted when fixing merge conflict

--- test/unit/test_diagram.py
+++ test/unit/test_diagram.py
@@ -17,9 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Autosubmit.  If not, see <http://www.gnu.org/licenses/>.
 
-
 """ Test file for autosubmit/monitor/diagram.py """
 
+import tempfile
 import datetime
 from pathlib import Path
 
@@ -34,7 +34,6 @@ from autosubmit.monitor.diagram import JobData, JobAggData
 def test_job_data():
     """ function to test the Class JobData inside autosubmit/monitor/diagram.py """
     job_data = JobData()
-
 
     assert job_data.headers() == ['Job Name', 'Queue Time', 'Run Time', 'Status']
     assert job_data.values() == ['', datetime.timedelta(0), datetime.timedelta(0), '']
