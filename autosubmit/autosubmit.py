e72683454c073066b3b8900e6733f433a2479acb
VindeeR <ericklopes.47@gmail.com>
2025-03-06 15:10:49 +0100

    adjusting all 100% trusted vulture not used, all tests were executed and approved too many lint errors to adjust in a single commit

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -97,23 +97,17 @@ sys.path.insert(0, os.path.abspath('.'))
 # noinspection PyUnusedLocal
 
 
-def signal_handler(signal_received, frame):
+def signal_handler():
     """
     Used to handle interrupt signals, allowing autosubmit to clean before exit
-
-    :param signal_received:
-    :param frame:
     """
     Log.info('Autosubmit will interrupt at the next safe occasion')
     Autosubmit.exit = True
 
 
-def signal_handler_create(signal_received, frame):
+def signal_handler_create():
     """
     Used to handle KeyboardInterrupt signals while the create method is being executed
-
-    :param signal_received:
-    :param frame:
     """
     raise AutosubmitCritical(
         'Autosubmit has been closed in an unexpected way. Killed or control + c.', 7010)
@@ -126,7 +120,7 @@ class MyParser(argparse.ArgumentParser):
         sys.exit(2)
 
 class CancelAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
+    def __call__(self, parser, namespace, values):
         setattr(namespace, self.dest, True)
         if namespace.filter_status.upper() == "SUBMITTED, QUEUING, RUNNING " or namespace.target.upper() == "FAILED":
             pass
@@ -2788,7 +2782,7 @@ class Autosubmit:
         return True
 
     @staticmethod
-    def statistics(expid, filter_type, filter_period, file_format, section_summary, jobs_summary, hide, notransitive=False, db = False):
+    def statistics(expid, filter_type, filter_period, file_format, section_summary, jobs_summary, hide, notransitive=False):
         """
         Plots statistics graph for a given experiment.
         Plot is created in experiment's plot folder with name <expid>_<date>_<time>.<file_format>
@@ -2807,8 +2801,6 @@ class Autosubmit:
         :type hide: bool
         :param notransitive: Reduces workflow linkage complexity
         :type hide: bool
-        :param db: Use database to get the statistics
-        :type db: bool
         """
         from .monitor.monitor import Monitor
 
