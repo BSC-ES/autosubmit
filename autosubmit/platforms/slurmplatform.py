33f0b7584e0ee138b52f82529dfe5af8c87e8100
dbeltran <daniel.beltran@bsc.es>
2025-03-13 12:39:59 +0100

    Fix Tests

--- autosubmit/platforms/slurmplatform.py
+++ autosubmit/platforms/slurmplatform.py
@@ -254,7 +254,7 @@ class SlurmPlatform(ParamikoPlatform):
             if self.send_command(cmd, x11=x11):
                 job_id = self.get_submitted_job_id(self.get_ssh_output(), x11=x11)
                 if job:
-                    Log.result(f"Job: {job.name} submitted with job_id: {job_id[:-1]} and workflow commit: {job.workflow_commit}")
+                    Log.result(f"Job: {job.name} submitted with job_id: {str(job_id).strip()} and workflow commit: {job.workflow_commit}")
                 return int(job_id)
             else:
                 return None
