b7b8d467f466d51e01762ce610b2f86628595dbd
dbeltran <daniel.beltran@bsc.es>
2025-03-27 12:49:00 +0100

    Automatic tests working, some failing as expected

--- test/regression/test_workflow_dependencies.py
+++ test/regression/test_workflow_dependencies.py
@@ -10,6 +10,7 @@ from test.unit.utils.common import create_database, init_expid
 import difflib
 
 PROFILE = False  # Enable/disable profiling ( speed up the tests )
+SHOW_WORKFLOW_PLOT = True  # Enable only for debugging purposes
 
 def prepare_custom_config_tests(default_yaml_file: Dict[str, Any], project_yaml_files: Dict[str, Dict[str, str]], current_tmpdir: Path) -> Dict[str, Any]:
     """
@@ -105,8 +106,80 @@ def prepare_workflow_runs(current_tmpdir: Path) -> Path:
             as_tmp.joinpath("ASLOGS").mkdir(parents=True, exist_ok=True)
 
 
+class SimpleJoblist:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def __str__(self):
+        return self.name
+
+
+def parse_job_list(lines):
+    roots = []
+    stack = []
+
+    for line in lines:
+        indent_level = line.count('|  ')
+        line = line.replace('|  ', '').replace('~ ', '').strip()
+        name = line.rsplit(' ')[0].strip("\n")
+
+        node = SimpleJoblist(name)
+
+        if indent_level == 0:
+            roots.append(node)
+        else:
+            while len(stack) > indent_level:
+                stack.pop()
+            stack[-1].add_child(node)
+
+        stack.append(node)
+
+    return sorted(roots, key=lambda x: x.name)
+
+
+def compare_and_print_differences(node1, node2):
+    differences = []
+    path = ""
+    stack = [(node1, node2, path)]
+
+    while stack:
+        n1, n2, current_path = stack.pop()
+
+        if n1 is None and n2 is None:
+            continue
+        if n1 is None or n2 is None:
+            differences.append(f"Difference at {current_path}: One of the nodes is None")
+            continue
+        if n1.name != n2.name:
+            differences.append(f"{current_path}: {n1.name} != {n2.name}")
+        if len(n1.children) != len(n2.children):
+            differences.append(
+                f"{current_path}: Number of children differ ({len(n1.children)} != {len(n2.children)})")
+
+        sorted_children1 = sorted(n1.children, key=lambda x: x.name)
+        sorted_children2 = sorted(n2.children, key=lambda x: x.name)
+
+        for child1, child2 in zip(sorted_children1, sorted_children2):
+            stack.append((child1, child2, current_path + "/" + n1.name[-10:]))
+
+    return differences
+
+
+def remove_noise_from_list(lines):
+    lines = [line.strip().rstrip(' [WAITING]').rstrip(' [READY]').strip() for line in lines]
+    lines = [line.replace("child", "children") if "child" in line and "children" not in line else line for line in lines]
+    if lines and lines[0].strip() == '':
+        lines = lines[1:]
+
+    return lines
+
+
 # By default, the test will be performed on all the workflows in the 'workflows' directory
-workflow_folders = [f.name for f in Path('./workflows').iterdir() if f.is_dir() and "pycache" not in f.name]
+workflow_folders = sorted([f.name for f in Path('./workflows').iterdir() if f.is_dir() and "pycache" not in f.name])
 
 
 @pytest.mark.parametrize("expid", workflow_folders)
@@ -122,31 +195,33 @@ def test_workflows_dependencies(prepare_workflow_runs, expid, current_tmpdir: Pa
 
     init_expid(os.environ["AUTOSUBMIT_CONFIGURATION"], platform='local', expid=expid, create=True, test_type='test')
 
-    with open(Path(f"{current_tmpdir}/workflows/{expid}/ref_workflow.txt")) as ref_file, \
-            open(Path(f"{current_tmpdir}/workflows/{expid}/tmp/ASLOGS/jobs_active_status.log"), "r") as new_file:
-        ref_lines = [line.rstrip(' [WAITING][READY]').strip() for line in ref_file.readlines()]
-        new_lines = [line.rstrip(' [WAITING][READY]').strip() for line in new_file.readlines()]
-        if ref_lines and ref_lines[0].strip() == '':
-            ref_lines = ref_lines[1:]
-        if ref_lines and ref_lines[-1].strip() == '':
-            ref_lines = ref_lines[:-1]
+    with open(Path(f"{current_tmpdir}/workflows/{expid}/ref_workflow.txt")) as ref_file:
+        ref_lines = remove_noise_from_list(ref_file.readlines())
 
-        if new_lines and new_lines[0].strip() == '':
-            new_lines = new_lines[1:]
-        if new_lines and new_lines[-1].strip() == '':
-            new_lines = new_lines[:-1]
-        diff = difflib.unified_diff(ref_lines, new_lines, lineterm='')
+    with open(Path(f"{current_tmpdir}/workflows/{expid}/tmp/ASLOGS/jobs_active_status.log"), "r") as new_file:
+        new_lines = remove_noise_from_list(new_file.readlines())
 
+    new_file_nodes = parse_job_list(new_lines[1:])
+    ref_file_nodes = parse_job_list(ref_lines[1:])
 
-    diff_list = list(diff)
-    if diff_list:
-        output_if_failed = "Differences\n"
-        output_if_failed += '\n'.join(diff_list)
-        output_if_failed += "Reference file:\n"
-        output_if_failed += '\n'.join(ref_lines)
-        output_if_failed += "New file:\n"
-        output_if_failed += '\n'.join(new_lines)
-        pytest.fail(output_if_failed)
+    differences = []
+    if len(new_file_nodes) != len(ref_file_nodes):
+        differences.append(f"Number of roots differ: {len(new_file_nodes)} != {len(ref_file_nodes)}")
+    if len(new_file_nodes) > len(ref_file_nodes):
+        new_file_nodes = new_file_nodes[:len(ref_file_nodes)]
+    else:
+        ref_file_nodes = ref_file_nodes[:len(new_file_nodes)]
+
+    for new_root, ref_root in zip(new_file_nodes, ref_file_nodes):
+        if new_root.name != ref_root.name:
+            differences.append(f"Difference at root: {new_root.name} != {ref_root.name}")
+        else:
+            differences.extend(compare_and_print_differences(new_root, ref_root))
+
+    if differences:
+        if SHOW_WORKFLOW_PLOT:
+            init_expid(os.environ["AUTOSUBMIT_CONFIGURATION"], platform='local', expid=expid, create=True, test_type='test', plot=SHOW_WORKFLOW_PLOT)
+        pytest.fail("\n".join(differences))
 
     if PROFILE:
         stats = pstats.Stats(profiler).sort_stats('cumtime')
