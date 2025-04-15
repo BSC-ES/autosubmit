1bc3aa4901570f18282bffb2149b14d5e97e575b
Irene Simo Munoz <irene.simomunoz@bsc.es>
2025-04-15 10:27:37 +0200

    Fix missed merge conflict marker

--- autosubmit/git/autosubmit_git.py
+++ autosubmit/git/autosubmit_git.py
@@ -287,4 +287,3 @@ class AutosubmitGit:
                     if any(status.startswith(code) for code in ["M", "A", "D", "?"] for status in output.splitlines()):
                         # M: Modified, A: Added, D: Deleted, ?: Untracked
                         raise AutosubmitCritical("Push local changes to remote repository before running", 7075)
-
