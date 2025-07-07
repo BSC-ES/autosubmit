250606e1ad0865b32b0f002ec3457dc1baed2b87
Irene Simo Munoz <irene.simomunoz@bsc.es>
2025-07-07 10:28:02 +0200

    git push -f Add file attachment for FAILED status update

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -2016,7 +2016,7 @@ class Autosubmit:
             exp_history = Autosubmit.get_historical_database(expid, job_list, as_conf)
             # establish the connection to all platforms
             # Restore is misleading, it is actually a "connect" function when the recover flag is not set.
-            Autosubmit.restore_platforms(job_list.submitter.platforms_object, as_conf=as_conf)
+            restore_platforms(job_list.submitter.platforms_object, as_conf=as_conf)
             return job_list, submitter, exp_history, host, as_conf, job_list.submitter.platforms_object, False
         return job_list, submitter, None, None, as_conf, job_list.submitter.platforms_object, True
 
@@ -2288,7 +2288,7 @@ class Autosubmit:
                                 else:
                                     mail_notify = False
                                 times = times + 1
-                                Autosubmit.restore_platforms(platforms_to_test, mail_notify=mail_notify,
+                                restore_platforms(platforms_to_test, mail_notify=mail_notify,
                                                              as_conf=as_conf, expid=expid)
                                 reconnected = True
                             except AutosubmitCritical as e:
@@ -3441,6 +3441,7 @@ class Autosubmit:
                     if smtp_hostname is not None or mail_from is not None:
                         parser.add_section('mail')
                         parser.set('mail', 'smtp_server', smtp_hostname)
+                        parser.set('mail', 'attachment', 'False')
                         parser.set('mail', 'mail_from', mail_from)
                     parser.add_section("globallogs")
                     parser.set("globallogs", "path", str(global_logs_path))
@@ -4928,7 +4929,7 @@ class Autosubmit:
                 definitive_platforms = []
                 for platform in platforms_to_test:
                     try:
-                        Autosubmit.restore_platforms([platform], as_conf=as_conf)
+                        restore_platforms([platform], as_conf=as_conf)
                         definitive_platforms.append(platform.name)
                     except Exception:
                         pass
