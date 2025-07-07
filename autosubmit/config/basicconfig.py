709878223635585c1bc7cf869714d963a09d14ad
Irene Simo Munoz <irene.simomunoz@bsc.es>
2025-07-07 10:28:02 +0200

    Add file attachment for FAILED status update

--- autosubmit/config/basicconfig.py
+++ autosubmit/config/basicconfig.py
@@ -60,6 +60,7 @@ class BasicConfig:
     CUSTOM_PLATFORMS_PATH = ''
     DEFAULT_JOBS_CONF = ''
     SMTP_SERVER = ''
+    ATTACHMENT = ''
     MAIL_FROM = ''
     ALLOWED_HOSTS: Union[str, dict] = ''
     DENIED_HOSTS: Union[str, dict] = ''
@@ -131,6 +132,8 @@ class BasicConfig:
             BasicConfig.DEFAULT_JOBS_CONF = parser.get('conf', 'jobs')
         if parser.has_option('mail', 'smtp_server'):
             BasicConfig.SMTP_SERVER = parser.get('mail', 'smtp_server')
+        if parser.has_option('mail', 'attachment'):
+            BasicConfig.ATTACHMENT = parser.get('mail', 'attachment')
         if parser.has_option('mail', 'mail_from'):
             BasicConfig.MAIL_FROM = parser.get('mail', 'mail_from')
         if parser.has_option('hosts', 'authorized'):
