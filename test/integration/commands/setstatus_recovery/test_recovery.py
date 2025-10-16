8c5a6b2f574cb0ae74e0ace9acf5aa9805e70e11
dbeltran <daniel.beltran@bsc.es>
2025-10-16 15:56:54 +0200

    WIP changes

---
+++ test/integration/commands/setstatus_recovery/test_recovery.py
@@ -0,0 +1,19 @@
+import pytest
+@pytest.mark.slurm
+def test_recovery(autosubmit_exp, general_data, experiment_data, jobs_data):
+    """
+    Test the recovery of an experiment.
+
+    :param as_exp: An AutosubmitExperiment instance.
+    :type as_exp: AutosubmitExperiment
+    :param general_data: General configuration data.
+    :type general_data: Dict[str, object]
+    :param experiment_data: Experiment-specific configuration data.
+    :type experiment_data: Dict[str, object]
+    :param jobs_data: Job-specific configuration data.
+    :type jobs_data: Dict[str, object]
+    """
+    config_data = general_data | experiment_data | jobs_data
+
+    as_exp = autosubmit_exp(experiment_data=config_data, include_jobs=False, create=True)
+    # Todo the actual test
