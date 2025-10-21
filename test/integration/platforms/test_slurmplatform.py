713fa2a0df836d882b3146292ba753415bd3b0b3
Luiggi Tenorio <luiggi.tenorio@bsc.es>
2025-10-21 15:47:42 +0200

    add wrapper test

--- test/integration/platforms/test_slurmplatform.py
+++ test/integration/platforms/test_slurmplatform.py
@@ -24,6 +24,7 @@ import pytest
 
 from autosubmit.config.configcommon import AutosubmitConfig
 from autosubmit.platforms.paramiko_submitter import ParamikoSubmitter
+from autosubmit.history.experiment_history import ExperimentHistory
 from autosubmit.log.utils import is_gzip_file, is_xz_file
 from autosubmit.platforms.slurmplatform import SlurmPlatform
 
@@ -776,20 +777,54 @@ def test_check_remote_permissions(autosubmit_exp, slurm_server: 'DockerContainer
                 },
             },
         },
+        {
+            "JOBS": {
+                "SIM": {
+                    "PLATFORM": _PLATFORM_NAME,
+                    "RUNNING": "once",
+                    "SCRIPT": 'echo "This is job ${SLURM_JOB_ID} EOM"',
+                },
+            },
+            "PLATFORMS": {
+                _PLATFORM_NAME: {
+                    "ADD_PROJECT_TO_HOST": False,
+                    "HOST": "127.0.0.1",
+                    "MAX_WALLCLOCK": "00:03",
+                    "PROJECT": "group",
+                    "QUEUE": "gp_debug",
+                    "SCRATCH_DIR": "/tmp/scratch/",
+                    "TEMP_DIR": "",
+                    "TYPE": "slurm",
+                    "USER": "root",
+                    "COMPRESS_REMOTE_LOGS": True,
+                    "PROCESSORS_PER_NODE": 1,
+                    "MAX_PROCESSORS": 1,
+                },
+            },
+            "WRAPPERS": {
+                "WRAPPER": {
+                    "TYPE": "vertical",
+                    "JOBS_IN_WRAPPER": "SIM",
+                }
+            },
+        },
     ],
     ids=[
         "Compress logs with default gzip",
         "Compress logs with xz",
         "Compress logs with default gzip in PS platform",
+        "Compress logs with gzip and vertical wrapper",
     ],
 )
 def test_simple_workflow_compress_logs_slurm(
-    autosubmit_exp: 'AutosubmitExperimentFixture',
+    autosubmit_exp: "AutosubmitExperimentFixture",
     experiment_data: dict,
     slurm_server: "DockerContainer",
 ):
     """Test compressing remote logs in a simple workflow using Slurm."""
-    exp = autosubmit_exp(_EXPID, experiment_data=experiment_data)
+    with_wrapper = "WRAPPERS" in experiment_data
+
+    exp = autosubmit_exp(_EXPID, experiment_data=experiment_data, wrapper=with_wrapper)
     _create_slurm_platform(exp.expid, exp.as_conf)
 
     exp.autosubmit._check_ownership_and_set_last_command(exp.as_conf, exp.expid, "run")
@@ -805,15 +840,38 @@ def test_simple_workflow_compress_logs_slurm(
         .get("REMOTE_LOGS_COMPRESS_TYPE", "gzip")
     )
 
+    # Get all files in the logs directory
     files = [f for f in Path(logs_dir).glob("*")]
 
     assert len(files) > 0, f"No log files found in {logs_dir}"
 
+    # Get job_data
+    exp_history = ExperimentHistory(
+        exp.expid,
+        exp.as_conf.basic_config.JOBDATA_DIR,
+        exp.as_conf.basic_config.HISTORICAL_LOG_DIR,
+    )
+    last_job_data = exp_history.manager.get_all_last_job_data_dcs()
+    assert len(last_job_data) > 0, "No job data found after running the experiment."
+
+    logs_filenames = []
+    for job in last_job_data:
+        logs_filenames.extend([job.out, job.err])
+
     if compression_type == "xz":
+        _val_fn = is_xz_file
         assert any(is_xz_file(str(f)) for f in files), (
             "No compressed xz log files found."
         )
     else:
+        _val_fn = is_gzip_file
         assert any(is_gzip_file(str(f)) for f in files), (
             "No compressed gzip log files found."
+        )
+
+    for log_filename in logs_filenames:
+        log_path = logs_dir.joinpath(log_filename)
+        assert log_path.exists(), f"Log file {log_path} does not exist."
+        assert _val_fn(str(log_path)), (
+            f"Log file {log_path} is not compressed as expected."
         )
