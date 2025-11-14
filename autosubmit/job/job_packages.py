692a47a7a6680eb2057a7a3c250f1285c17efe66
dbeltrankyl <daniel.beltran@bsc.es>
2025-11-14 12:23:13 +0100

    feedback

--- autosubmit/job/job_packages.py
+++ autosubmit/job/job_packages.py
@@ -162,8 +162,7 @@ class JobPackageBase(object):
                     Log.warning(f"[section:{job.section}]: Job script:{job.file} does not exists, skipping check")
 
             for additional_file in job.additional_files:
-                exists = not additional_file or (project_dir / additional_file).exists()
-                if not exists:
+                if not additional_file or (project_dir / additional_file).exists():
                     if only_generate:
                         raise AutosubmitCritical(f"[section:{job.section}]: Additional file:{additional_file} does not exists", 7014)
                     Log.warning(f"[section:{job.section}]: Additional file:{additional_file} does not exists, skipping check")
