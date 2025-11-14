03c1cb0df3686fa6eebda9c020ad5b024677d082
pgoitia <pablo.goitia@bsc.es>
2025-11-14 16:41:34 +0100

    Now jobs inside a wrapper contains more wrapper information

--- autosubmit/job/job_packages.py
+++ autosubmit/job/job_packages.py
@@ -28,7 +28,7 @@ import time
 from contextlib import suppress
 from datetime import timedelta
 from threading import Thread
-from typing import Optional, TYPE_CHECKING
+from typing import List, Optional, TYPE_CHECKING
 
 from bscearth.utils.date import sum_str_hours
 
@@ -87,6 +87,7 @@ class JobPackageBase(object):
             for job in jobs:
                 if job._platform.name != self._platform.name or job.platform is None:
                     raise Exception('Only one valid platform per package')
+                # TODO: [ENGINES] Here I could assign the wrapper method to the jobs in the package
         except IndexError:
             raise Exception('No jobs given')
 
@@ -157,7 +158,7 @@ class JobPackageBase(object):
                 Log.result("Script {0} OK", job.name)
             # looking for directives on jobs
             self._custom_directives = self._custom_directives | set(job.custom_directives)
-        self._create_scripts(configuration) # TODO: [ENGINES] Duplicated. Solved by Dani in PR #2693, pending review
+        # self._create_scripts(configuration) # TODO: [ENGINES] Duplicated. Solved by Dani in PR #2693, pending review
 
     def submit(self, configuration: 'AutosubmitConfig', parameters: Optional[dict] = None, only_generate: bool = False, hold: bool = False):
         """
@@ -232,6 +233,16 @@ class JobPackageBase(object):
     def _do_submission(self, job_scripts=None, hold: bool = False):
         """ Submit package to the platform. """
         pass  # pragma: no cover
+    
+    def _assign_wrapper_method_to_jobs(self, jobs: List[Job], wrapper_method: str) -> None:
+        """
+        Assign the wrapper method to each job in the list.
+
+        :param jobs: List of jobs.
+        :param wrapper_method: Wrapper method to be assigned.
+        """
+        for job in jobs:
+            job.wrapper_method = wrapper_method
 
     def process_jobs_to_submit(self, job_id: str, hold: bool = False) -> None:
         for i, job in enumerate(self.jobs):
@@ -257,6 +268,9 @@ class JobPackageSimple(JobPackageBase):
         # TODO: This should be possible, but it crashes across the code.
         #  Add a property that defines what is a package with wrappers
         # self.name = "simple_package"
+
+        # TODO: [ENGINES] Simple jobs may have been in a wrapper previously, so we reset the wrapper method.
+        self._assign_wrapper_method_to_jobs(jobs, None)
 
     def _create_scripts(self, configuration: 'AutosubmitConfig'):
         for job in self.jobs:
@@ -454,8 +468,7 @@ class JobPackageThread(JobPackageBase):
         self._common_script = None
         self.executable = None
 
-        self._wallclock = '00:00'
-        # depends on the type of wrapper
+        self._wallclock = '00:00' # depends on the type of wrapper
 
         self._jobs_resources = jobs_resources
         self._wrapper_factory = self.platform.wrapper # TODO: [ENGINES] This is where the wrapper factory is assigned
@@ -554,6 +567,9 @@ class JobPackageThread(JobPackageBase):
         self.parameters['TASKS'] = self.tasks
         self.parameters["EXECUTABLE"] = self.executable  # have to look
         self.method = method
+
+        # Assign wrapper method to jobs so that they can be assigned their platform or the wrapper engine later
+        self._assign_wrapper_method_to_jobs(jobs, method)
 
     @property
     def name(self):
@@ -859,9 +875,9 @@ class JobPackageHorizontal(JobPackageThread):
     Class to manage a horizontal thread-based package of jobs to be submitted by autosubmit
     """
     def __init__(self, jobs: list[Job], dependency=None, jobs_resources: dict = None, method: str = 'ASThread',
-                 configuration: Optional['AutosubmitConfig'] = None, wrapper_section="WRAPPERS"):
+                 configuration: Optional['AutosubmitConfig'] = None, wrapper_section="WRAPPERS", wrapper_info: Optional[list] = None):
         super(JobPackageHorizontal, self).__init__(jobs, dependency, jobs_resources, configuration=configuration,
-                                                   wrapper_section=wrapper_section)
+                                                   wrapper_section=wrapper_section, wrapper_info=wrapper_info, method=method)
         self.method = method
         self._queue = self.queue
         for job in jobs:
@@ -888,17 +904,17 @@ class JobPackageHorizontal(JobPackageThread):
 
 class JobPackageHybrid(JobPackageThread):
     """
-        Class to manage a hybrid (horizontal and vertical) thread-based package of jobs to be submitted by autosubmit
-        """
+    Class to manage a hybrid (horizontal and vertical) thread-based package of jobs to be submitted by autosubmit
+    """
 
     def __init__(self, jobs: list[list[Job]], num_processors: str, total_wallclock, dependency=None,
-                 jobs_resources: Optional[dict] = None, method: str = "ASThread",
-                 configuration: Optional['AutosubmitConfig'] = None, wrapper_section="WRAPPERS"):
+                 jobs_resources: Optional[dict] = None, method: str = "ASThread", configuration: Optional['AutosubmitConfig'] = None,
+                 wrapper_section="WRAPPERS", wrapper_info: Optional[list] = None):
         all_jobs = [item for sublist in jobs for item in sublist]  # flatten list
         if jobs_resources is None:
             jobs_resources = {}
-        super(JobPackageHybrid, self).__init__(all_jobs, dependency, jobs_resources, method,
-                                               configuration=configuration, wrapper_section=wrapper_section)
+        super(JobPackageHybrid, self).__init__(all_jobs, dependency, jobs_resources, method, configuration=configuration, 
+                                               wrapper_section=wrapper_section, wrapper_info=wrapper_info)
         self.jobs_lists = jobs
         self.method = method
         self._num_processors = int(num_processors)
