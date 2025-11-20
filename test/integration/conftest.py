9a3ef239a03e709c136eb9b12a92edd3418ac50b
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2025-11-20 14:44:09 +0100

    Avoid loading user SSH config, load a fake SSH config file (so the fixture can change the port)

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -31,6 +31,7 @@ from pwd import getpwnam
 from re import sub
 from subprocess import check_output
 from tempfile import TemporaryDirectory
+from textwrap import dedent
 from time import time_ns
 from typing import cast, Any, ContextManager, Generator, Iterator, Optional, Protocol, Union, TYPE_CHECKING
 
@@ -459,11 +460,21 @@ def ssh_server(mocker, tmp_path, request) -> Generator[DockerContainer, None, No
         ssh_client = _make_ssh_client(ssh_port, _SSH_DOCKER_PASSWORD, None, mfa)
         mocker.patch('autosubmit.platforms.paramiko_platform._create_ssh_client', return_value=ssh_client)
 
+        ssh_config = Path(tmp_path, '.ssh/ssh_config')
+        ssh_config.parent.mkdir(exist_ok=True, parents=True)
+        ssh_config.touch()
+        ssh_config.write_text(dedent(f"""\
+        Host localhost
+            Hostname localhost
+            User {user}
+            ForwardX11 yes
+            Port 22
+        """))
         if mfa:
             # It uses a Transport and not an SSH client directly. Ideally, we would be able
             # to use just one way
             original_paramiko_config = paramiko.SSHConfig()
-            with open(Path('~/.ssh/config').expanduser()) as f:
+            with open(ssh_config) as f:
                 original_paramiko_config.parse(f)
             modified_config = original_paramiko_config.lookup('localhost')
             modified_config['port'] = f'{ssh_port}'
