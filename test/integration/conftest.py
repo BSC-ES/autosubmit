767b43f8e8014e205abc76be95e76da739669e3d
VindeeR <ericklopes.47@gmail.com>
2025-11-20 13:22:59 +0100

    Commit removing unused code spread over Autosubmit and adding fix for tests that broke integration test locally

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -334,6 +334,8 @@ def _make_ssh_client(ssh_port: int, password: Optional[str], key: Optional[Union
             kwargs['port'] = ssh_port
         if 'password' not in kwargs:
             kwargs['password'] = password
+            kwargs['look_for_keys'] = False
+            kwargs['allow_agent'] = False
         if len(args) > 1:
             # tuple to list, and then replace the port...
             args = [x for x in args]
