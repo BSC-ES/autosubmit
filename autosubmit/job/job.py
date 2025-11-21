0823ab3e2ecc50e7e662b3d9d9b147a5973977fc
pgoitia <pablo.goitia@bsc.es>
2025-11-21 14:30:09 +0100

    Environment setup is now available as a config parameter

--- autosubmit/job/job.py
+++ autosubmit/job/job.py
@@ -1637,6 +1637,7 @@ class Job(object):
                 "min_wrapped_h",
                 "min_wrapped_v",
                 "policy"
+                "custom_env_setup"
             ]:
                 parameters[f"CURRENT_{key.upper()}"] = value
 
