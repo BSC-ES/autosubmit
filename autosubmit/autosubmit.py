99681072ada4415c07fd6faa69b46ec7cccc0005
dbeltrankyl <daniel.beltran@bsc.es>
2025-11-21 14:36:08 +0100

    fix code

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -5287,6 +5287,7 @@ class Autosubmit:
 
         def _prune_jobs(jobs: list[Job], dates: list[str], members: list[str], chunks: int) -> list[Job]:
             """Return jobs from *jobs* that match the given members, dates and chunk limits.
+
             :param jobs: list of jobs to prune
             :param dates: list of dates to match
             :param members: list of members to match
@@ -5304,11 +5305,10 @@ class Autosubmit:
                         )
                         ]
 
-        final_list = []
-
         if not filter_chunks or not isinstance(filter_chunks, str):
             return []
 
+        final_list = []
         filter_chunks = filter_chunks.upper()
         matching_jobs = job_list.get_job_list()
         if "," in filter_chunks:
@@ -5425,9 +5425,8 @@ class Autosubmit:
                 Autosubmit._validate_set_status_filters(as_conf, job_list, filter_list, filter_chunks, filter_status,
                                                         filter_section, filter_type_chunk, filter_type_chunk_split)
                 #### Starts the filtering process ####
-                Log.info(f"Filtering jobs...")
+                Log.info("Filtering jobs...")
                 final_list = []
-                jobs_filtered = []
                 final_status = Autosubmit._get_status(final)
                 # I have the impression that whoever did this function thought about the possibility of having multiple filters at the same time
                 # But, as it was, it is not possible to have multiple filters at the same time due to the way the code is written
