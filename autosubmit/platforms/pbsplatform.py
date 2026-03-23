35bef49e36c0940e83969bba8a8233f0e335ca52
VindeeR <ericklopes.47@gmail.com>
2026-03-23 08:59:44 +0100

    reactivating the wrappers first commit making the files being generated but not functional as it currently is

--- autosubmit/platforms/pbsplatform.py
+++ autosubmit/platforms/pbsplatform.py
@@ -293,7 +293,6 @@ class PBSPlatform(ParamikoPlatform):
         """Get the variable remote_log_dir that stores the directory of the Log of the experiment.
 
         :return: The remote_log_dir variable.
-        :rtype: str
         """
         return self.remote_log_dir
 
@@ -305,7 +304,6 @@ class PBSPlatform(ParamikoPlatform):
         :param job_id: job ID.
         :type job_id: int
         :return: All status related to a Job.
-        :rtype: str
         """
         with suppress(Exception):
             output_lines = output.lower().split('\n')
@@ -344,10 +342,8 @@ class PBSPlatform(ParamikoPlatform):
         """Generate qstat command for all the jobs passed down.
 
         :param jobs_id: ID of one or more jobs.
-        :param jobs_id: str
 
         :return: qstat command to all jobs.
-        :rtype: str
         """
         jobs_id = jobs_id.replace('{', '').replace('}', '').replace(',', ' ')
         return f"qstat {jobs_id} | awk" + " '{print $1, $3}' && " + f"qstat -H {jobs_id} | awk" + " '{print $1, $3}'"
@@ -356,9 +352,8 @@ class PBSPlatform(ParamikoPlatform):
         """Get an estimated queue time to the job selected.
 
         :param job_id: ID of a job.
-        :param job_id: str
+
         :return: Gets estimated queue time.
-        :rtype: str
         """
         job_id = job_id.replace('{', '').replace('}', '').replace(',', ' ')
         return f"qstat -f {job_id} | grep 'eligible_time = [0-9:0-9:0-9]*' && echo \"BREAK\" && " + f"qstat -H -f {job_id} | grep 'eligible_time = [0-9:0-9:0-9]*'"
@@ -369,11 +364,9 @@ class PBSPlatform(ParamikoPlatform):
         """Parse the queue reason from the output of the command.
 
         :param output: output of the command.
-        :type output: str
         :param job_id: job id
-        :type job_id: str
+
         :return: queue reason.
-        :rtype: str
         """
         reason = [x.split(',')[1] for x in output.splitlines()
                   if x.split(',')[0] == str(job_id)]
@@ -388,9 +381,8 @@ class PBSPlatform(ParamikoPlatform):
         """Generate the header of the wrapper configuring it to execute the Experiment.
 
         :param kwargs: Key arguments associated to the Job/Experiment to configure the wrapper.
-        :type kwargs: Any
+
         :return: a sequence of PBS commands.
-        :rtype: str
         """
         return self._header.wrapper_header(**kwargs)
 
