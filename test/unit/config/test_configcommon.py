47a8d02c008a7c48edfd47fe983e7749ff909ac7
ntorqulu <nuria.torquet@bsc.es>
2026-03-30 15:06:41 +0200

    fix order of loading pre

--- test/unit/config/test_configcommon.py
+++ test/unit/config/test_configcommon.py
@@ -17,6 +17,7 @@
 
 """Basic tests for ``AutosubmitConfig``."""
 
+import copy
 from pathlib import Path
 from textwrap import dedent
 from typing import TYPE_CHECKING
@@ -596,17 +597,19 @@ def test_load_custom_config(autosubmit_config, tmp_path) -> None:
 
     as_conf.starter_conf = {
         "DEFAULT": {"EXPID": "a000", "HPCARCH": "LOCAL"},
-        "JOBS": {"DO_NOTHING": {"SCRIPT": "sleep 20'", "PLATFORM": "LOCAL", "RUNNING": "once"}},
+        "JOBS": {"DO_NOTHING": {"SCRIPT": "sleep 20", "PLATFORM": "LOCAL", "RUNNING": "once"}},
         "CONFIG": {},
         "PROJDIR": str(git_project_dir),
     }
 
     current_data = {
         "DEFAULT": {"EXPID": "a000", "HPCARCH": "LOCAL"},
-        "JOBS": {"DO_NOTHING": {"SCRIPT": "sleep 20'", "PLATFORM": "LOCAL", "RUNNING": "once"}},
+        "JOBS": {"DO_NOTHING": {"SCRIPT": "sleep 20", "PLATFORM": "LOCAL", "RUNNING": "once"}},
         "CONFIG": {},
         "PROJDIR": str(git_project_dir),
     }
+
+    current_data_before = copy.deepcopy(current_data)
 
     root_file = conf_dir / "root_config.yml"
     root_file.write_text(
@@ -668,8 +671,8 @@ def test_load_custom_config(autosubmit_config, tmp_path) -> None:
     assert "CUSTOM_CONFIG" not in data_post.get("DEFAULT", {})
 
     # check that pinned variables are not overwritten in data_pre
-    # assert data_pre["DEFAULT"]["EXPID"] == "a000"
-    # assert data_pre["DEFAULT"]["HPCARCH"] == "LOCAL"
+    assert data_pre["DEFAULT"]["EXPID"] == "a000"
+    assert data_pre["DEFAULT"]["HPCARCH"] == "LOCAL"
 
     # check that POST config is merged in data_post
     assert "POST_CONFIG_VALUE" not in data_pre.get("DEFAULT", {})
@@ -684,10 +687,10 @@ def test_load_custom_config(autosubmit_config, tmp_path) -> None:
     assert "POST_ONLY_TMP" not in data_pre["DEFAULT"]
 
     # check input current_data is not mutated by load_custom_config
-    # assert current_data == current_data_before
+    assert current_data == current_data_before
 
     # check that JOBS section has DO_NOTHING script with post b.yml!
     assert data_post["JOBS"]["DO_NOTHING"]["SCRIPT"] == "post b.yml!"
-    assert data_pre["JOBS"]["DO_NOTHING"]["SCRIPT"] == "pre a.yml!"
+    assert data_pre["JOBS"]["DO_NOTHING"]["SCRIPT"] == "sleep 20"
 
     assert data_pre is not data_post
