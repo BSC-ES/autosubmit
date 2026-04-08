a94c22af2e18254b8447954ab7fe1da36da28d6a
Pablo Aparici <pablo.aparici@bsc.es>
2026-04-08 12:05:02 +0200

    Enhancement: Detect inefficiencies in SIM jobs with CPMIPS thresholds - Part 2 (#2918)
    * Add new attributes in Job for computation of CPMIPS.
    
    * Delete comma
    
    * Delete unnecessary test that checks if the chunk size and chunk size unit of an experiment is changed in the job config. Add comments for the paramatized tests.
    
    * Refactor the collection of chunk size and chunksize unit due to the deletion of the unnecessary test. Moreover, add comments in the variables. And correct the type of the variables in the update function (they are private).
    
    * Add tests for split jobs
    
    * Update year copyright
    
    ---------
    
    Co-authored-by: Erick Lopes <30758033+VindeeR@users.noreply.github.com>

--- autosubmit/job/job.py
+++ autosubmit/job/job.py
@@ -148,7 +148,7 @@ class Job(object):
         'delete_when_edgeless', 'het', 'updated_log',
         'submit_time_timestamp', 'start_time_timestamp', 'finish_time_timestamp',
         '_script', '_log_recovery_retries', 'ready_date', 'wrapper_name',
-        'is_wrapper', '_wallclock_in_seconds', '_notify_on', '_processors_per_node',
+        'is_wrapper', '_wallclock_in_seconds', '_notify_on', '_cpmip_thresholds', '_chunk_size', '_chunk_size_unit', '_processors_per_node',
         'ec_queue', 'platform_name', '_serial_platform',
         'submitter', '_shape', '_x11', '_x11_options', '_hyperthreading',
         '_scratch_free_space', '_delay_retrials', '_custom_directives',
@@ -282,6 +282,11 @@ class Job(object):
         self.is_wrapper = False
         self._wallclock_in_seconds = None
         self._notify_on = None
+        # The three variables under this message are related to the #PR2918 that is a development
+        # focused on adding the key information for computing the simulated years for the CPMIPS metrics.
+        self._cpmip_thresholds = {} 
+        self._chunk_size = None 
+        self._chunk_size_unit = None 
         self._processors_per_node = None
         self.ec_queue = None
         self.platform_name = None
@@ -342,6 +347,9 @@ class Job(object):
         self.is_wrapper = False
         self._wallclock_in_seconds = None
         self._notify_on = None
+        self._cpmip_thresholds = {}
+        self._chunk_size = None
+        self._chunk_size_unit = None
         self._processors_per_node = None
         self._shape = None
         self._x11 = False
@@ -690,6 +698,36 @@ class Job(object):
     @notify_on.setter
     def notify_on(self, value):
         self._notify_on = value
+
+    @property
+    @autosubmit_parameter(name='cpmip_thresholds')
+    def cpmip_thresholds(self):
+        """Thresholds for CPMIP metrics."""
+        return self._cpmip_thresholds
+
+    @cpmip_thresholds.setter
+    def cpmip_thresholds(self, value):
+        self._cpmip_thresholds = value
+
+    @property
+    @autosubmit_parameter(name='chunk_size')
+    def chunk_size(self):
+        """Chunk size used to compute CPMIP metrics."""
+        return self._chunk_size
+
+    @chunk_size.setter
+    def chunk_size(self, value):
+        self._chunk_size = value
+
+    @property
+    @autosubmit_parameter(name='chunk_size_unit')
+    def chunk_size_unit(self):
+        """Chunk size unit used to compute CPMIP metrics."""
+        return self._chunk_size_unit
+
+    @chunk_size_unit.setter
+    def chunk_size_unit(self, value):
+        self._chunk_size_unit = value
 
     @property
     @autosubmit_parameter(name='validate_template')
@@ -1984,6 +2022,9 @@ class Job(object):
         self.ext_tailer_path = as_conf.jobs_data.get(self.section, {}).get('EXTENDED_TAILER_PATH', None)
         if self.platform_name:
             self.platform_name = self.platform_name.upper()
+        self._cpmip_thresholds = as_conf.jobs_data.get(self.section, {}).get("CPMIP_THRESHOLDS", {})
+        self._chunk_size = as_conf.get_chunk_size()
+        self._chunk_size_unit = as_conf.get_chunk_size_unit().lower()
 
     def update_check_variables(self, as_conf: AutosubmitConfig) -> None:
         job_data = as_conf.jobs_data.get(self.section, {})
---
+++ test/unit/test_split_job.py
@@ -0,0 +1,197 @@
+# Copyright 2015-2026 Earth Sciences Department, BSC-CNS
+#
+# This file is part of Autosubmit.
+#
+# Autosubmit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Autosubmit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Autosubmit.  If not, see <http://www.gnu.org/licenses/>.
+
+import copy
+from datetime import datetime
+
+import pytest
+
+from autosubmit.job.job_list import JobList
+from autosubmit.job.job_list_persistence import JobListPersistencePkl
+from autosubmit.job.template import Language
+from autosubmit.platforms.psplatform import PsPlatform
+
+
+def _build_split_experiment_data(experiment_data):
+    split_experiment_data = copy.deepcopy(experiment_data)
+    split_job_data = split_experiment_data.setdefault('JOBS', {}).setdefault('RANDOM-SECTION', {})
+    split_job_data['RUNNING'] = 'chunk'
+
+    split_experiment_data.setdefault('PLATFORMS', {}).setdefault('dummy_platform', {'type': 'ps'})
+    split_experiment_data.setdefault('ROOTDIR', 'dummy_rootdir')
+    split_experiment_data.setdefault('LOCAL_TMP_DIR', 'dummy_tmpdir')
+    split_experiment_data.setdefault('LOCAL_ROOT_DIR', 'dummy_rootdir')
+
+    return split_experiment_data
+
+
+def _generate_and_get_split_job(autosubmit_config, experiment_data):
+    as_conf = autosubmit_config('t000', experiment_data)
+    as_conf.experiment_data = as_conf.deep_normalize(as_conf.experiment_data)
+    as_conf.experiment_data = as_conf.normalize_variables(as_conf.experiment_data, must_exists=True)
+    as_conf.experiment_data = as_conf.deep_read_loops(as_conf.experiment_data)
+    as_conf.experiment_data = as_conf.substitute_dynamic_variables(as_conf.experiment_data)
+    as_conf.experiment_data = as_conf.parse_data_loops(as_conf.experiment_data)
+
+    parameters = as_conf.load_parameters()
+    job_list_obj = JobList('t000', as_conf, as_conf.parser_factory, JobListPersistencePkl())
+    job_list_obj.generate(
+        as_conf=as_conf,
+        date_list=[datetime(2020, 1, 1)],
+        member_list=['fc0'],
+        num_chunks=1,
+        chunk_ini=1,
+        parameters=parameters,
+        date_format=None,
+        default_retrials=as_conf.get_retrials(),
+        default_job_type=Language.BASH,
+        wrapper_jobs={},
+        new=True,
+        run_only_members=[],
+        show_log=False,
+        create=True,
+    )
+
+    split_jobs = [
+        job for job in job_list_obj.get_job_list()
+        if job.section == 'RANDOM-SECTION' and isinstance(job.split, int) and job.split > 0
+    ]
+    assert split_jobs
+
+    for job in split_jobs:
+        job.platform = PsPlatform(expid='t000', name='DUMMY_PLATFORM', config=as_conf.experiment_data)
+        job.update_parameters(as_conf, set_attributes=True)
+    return split_jobs
+
+
+@pytest.mark.parametrize('experiment_data, attributes_to_check, configured_splits', [
+    (
+        {
+            'JOBS': {
+                'RANDOM-SECTION': {
+                    'FILE': 'test.sh',
+                    'PLATFORM': 'DUMMY_PLATFORM',
+                    'NOTIFY_ON': 'COMPLETED',
+                    'SPLITS': 2,
+                },
+            },
+            'PLATFORMS': {
+                'dummy_platform': {
+                    'type': 'ps',
+                },
+            },
+            'ROOTDIR': 'dummy_rootdir',
+            'LOCAL_TMP_DIR': 'dummy_tmpdir',
+            'LOCAL_ROOT_DIR': 'dummy_rootdir',
+        },
+        {'notify_on': ['COMPLETED']},
+        2
+    ),
+    (
+        {
+            'JOBS': {
+                'RANDOM-SECTION': {
+                    'FILE': 'test.sh',
+                    'PLATFORM': 'DUMMY_PLATFORM',
+                    'CPMIP_THRESHOLDS': {
+                        'SYPD': {
+                            'THRESHOLD': 5.0,
+                            'COMPARISON': 'greater_than',
+                            '%_ACCEPTED_ERROR': 10,
+                        }
+                    },
+                    'SPLITS': 3,
+                },
+            },
+        },
+        {
+            'cpmip_thresholds': {
+                'SYPD': {
+                    'THRESHOLD': 5.0,
+                    'COMPARISON': 'greater_than',
+                    '%_ACCEPTED_ERROR': 10,
+                }
+            }
+        },
+        3
+    ),
+    (
+        {
+            'JOBS': {
+                'RANDOM-SECTION': {
+                    'FILE': 'test.sh',
+                    'PLATFORM': 'DUMMY_PLATFORM',
+                    'SPLITS': 1,
+                },
+            },
+        },
+        {'cpmip_thresholds': {}},
+        1
+    ),
+    (
+        {
+            'EXPERIMENT': {
+                'CHUNKSIZE': 3,
+                'CHUNKSIZEUNIT': 'MONTH',
+            },
+            'JOBS': {
+                'RANDOM-SECTION': {
+                    'FILE': 'test.sh',
+                    'PLATFORM': 'DUMMY_PLATFORM',
+                    'SPLITS': 4,
+                },
+            },
+        },
+        {'chunk_size': 3, 'chunk_size_unit': 'month'},
+        4
+    ),
+    (
+        {
+            'JOBS': {
+                'RANDOM-SECTION': {
+                    'FILE': 'test.sh',
+                    'PLATFORM': 'DUMMY_PLATFORM',
+                    'SPLITS': 5,
+                },
+            },
+        },
+        {'chunk_size': 1, 'chunk_size_unit': ''},
+        5
+    ),
+], ids=[
+    'notify_on_attribute',
+    'cpmip_thresholds_from_config',
+    'empty_cpmip_thresholds_when_missing',
+    'chunk_metadata_from_experiment_defaults',
+    'chunk_metadata_when_missing',
+])
+def test_update_parameters_split_attributes(autosubmit_config, experiment_data, attributes_to_check, configured_splits):
+    split_experiment_data = _build_split_experiment_data(experiment_data)
+    split_jobs = _generate_and_get_split_job(autosubmit_config, split_experiment_data)
+
+    assert len(split_jobs) == configured_splits
+    assert sorted(int(job.split) for job in split_jobs) == list(range(1, configured_splits + 1))
+
+    for job in split_jobs:
+        assert job.split > 0
+        assert job.splits > 0
+        assert int(job.splits) == configured_splits
+        assert 1 <= int(job.split) <= configured_splits
+
+        for attr, expected in attributes_to_check.items():
+            assert hasattr(job, attr)
+            assert getattr(job, attr) == expected
