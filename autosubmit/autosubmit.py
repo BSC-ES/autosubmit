1662d5966b969b4ec29d455e6a9453a41c5b0fbf
dbeltran <daniel.beltran@bsc.es>
2026-04-09 11:04:11 +0200

    adding profiler from 4.2.0 (i)

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -832,8 +832,10 @@ class Autosubmit:
         if args.command != "configure" and args.command != "install":
             Autosubmit._init_logs(args, args.logconsole, args.logfile, expid)
         if args.command == 'run':
+            if args.trace and not args.profile:
+                raise AutosubmitCritical('Tracing is only available with profiling. Please add -p/--profile flag to run with tracing.', 7012)
             return Autosubmit.run_experiment(args.expid, args.start_time, args.start_after, args.run_only_members,
-                                             args.profile)
+                                             args.profile, args.trace, args.profile_max_iterations)
         elif args.command == 'expid':
             return Autosubmit.expid(args.description, args.HPC, args.copy, args.dummy, args.minimal_configuration,
                                     args.git_repo, args.git_branch, args.git_as_conf, args.operational, args.testcase,
@@ -2063,7 +2065,7 @@ class Autosubmit:
 
     @staticmethod
     def run_experiment(expid: str, start_time: Optional[str] = None, start_after: Optional[str] = None,
-                       run_only_members: Optional[str] = None, profile=False) -> int:
+                       run_only_members: Optional[str] = None, profile: bool = False, trace: bool = False, profile_max_iterations: int = 0) -> int:
         """Runs and experiment (submitting all the jobs properly and repeating its execution in case of failure).
 
         :param expid: the experiment id
@@ -2071,14 +2073,19 @@ class Autosubmit:
         :param start_after: the expid after which the experiment should start
         :param run_only_members: the members to run
         :param profile: if True, the function will be profiled
+        :param trace: if True, the function will be traced
+        :param profile_max_iterations: the maximum number of iterations to run when profiling, 0 means no limit
         :return: exit status
 
         """
         # Start profiling if the flag has been used
         if profile:
             from .profiler.profiler import Profiler
-            profiler = Profiler(expid)
+            profiler = Profiler(expid, trace_enabled=trace, max_checkpoints=profile_max_iterations)
             profiler.start()
+            profiler.iteration_checkpoint(0, 0)
+        else:
+            profiler = None
 
         # Initialize common folders'
         try:
