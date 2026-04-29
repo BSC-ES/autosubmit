1864c83b63001942d6a4d26a2a49709b61f58444
ntorqulu <nuria.torquet@bsc.es>
2026-04-29 13:43:46 +0200

    do not store the experiment status on expid and create

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -1514,7 +1514,7 @@ class Autosubmit:
             Log.warning(f"Could not update experiment details for {exp_id}. Omitting this step.")
             Log.debug(f"Error calling save_update_details: {str(e)}")
 
-        ExperimentStatus(exp_id).set_as_not_running()
+        # ExperimentStatus(exp_id).set_as_not_running()
 
         Log.result(f"Experiment {exp_id} created")
         return exp_id
@@ -4104,7 +4104,7 @@ class Autosubmit:
                     Log.warning(
                         "Remember to MODIFY the MODEL config files!")
                     
-                    ExperimentStatus(expid).set_as_not_running()
+                    # ExperimentStatus(expid).set_as_not_running()
                     fh.flush()
                     os.fsync(fh.fileno())
                     if detail:
