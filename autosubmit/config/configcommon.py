7144f4266e2a662ad1cb6adcc4761700fb895141
ntorqulu <nuria.torquet@bsc.es>
2026-05-07 14:23:16 +0200

    set pinned_variables to lowercase

--- autosubmit/config/configcommon.py
+++ autosubmit/config/configcommon.py
@@ -1634,7 +1634,7 @@ class AutosubmitConfig(object):
         :rtype: dict
         """
         # Variables that should be fixed regardless of the configuration file
-        PINNED_VARIABLES = ["EXPID", "HPCARCH"]
+        pinned_variables = ["EXPID", "HPCARCH"]
 
         starter_default = self.starter_conf.get("DEFAULT", {})
 
@@ -1642,7 +1642,7 @@ class AutosubmitConfig(object):
         if starter_default:
             default_section = parameters.setdefault("DEFAULT", {})
 
-            for key in PINNED_VARIABLES:
+            for key in pinned_variables:
                 if key not in starter_default:
                     continue
                 # For each key, get the original value and the one in the configuration file
