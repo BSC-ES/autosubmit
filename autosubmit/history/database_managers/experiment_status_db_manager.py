54a722e6e9e72a319622401e9da599188c2d2060
ntorqulu <nuria.torquet@bsc.es>
2026-05-14 16:18:34 +0200

    fix ruff

--- autosubmit/history/database_managers/experiment_status_db_manager.py
+++ autosubmit/history/database_managers/experiment_status_db_manager.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Autosubmit.  If not, see <http://www.gnu.org/licenses/>.
 
-from ast import Add
 import os
 import textwrap
 from pathlib import Path
